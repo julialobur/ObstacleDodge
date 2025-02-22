b38744bbe263b573655d5a8162361e967aadfb12
julia <loburj@gmail.com>
2025-02-22 16:04:59 +0200

    collision, score, tags and time

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,12 +5,21 @@ public class Mover : MonoBehaviour
     [SerializeField] float moveSpeed = 10f;
     void Start()
     {
-
+        PrintInstructions();
     }
 
     void Update()
     {
+        MovePlayer();
+    }
 
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
